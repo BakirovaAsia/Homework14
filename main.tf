3836b15d9294d9c156afe232adbbb76587727427
BakirovaAsia <bakirova.asia@gmail.com>
2021-05-07 15:03:39 +0300

    v4_cidr_blocks

--- main.tf
+++ main.tf
@@ -73,6 +73,7 @@ resource "yandex_vpc_subnet" "subnet-1" {
   name           = "subnet1"
   zone           = "ru-central1-c"
   network_id     = yandex_vpc_network.network-1.id
+  v4_cidr_blocks = ["192.168.10.0/24"]
 }
 
 resource yandex_container_registry "my-registry" {
